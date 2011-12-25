730ba66bf168367e050494b6de4fc4798a95e8e2
James Pellerano <james@japellerano.com>
2011-12-25 10:27:03 -0500

    authorization to controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
-
+   load_and_authorize_resource
+   
    def create
       @post = Post.find(params[:post_id])
       @comment = @post.comments.create(params[:comment])
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-
+   load_and_authorize_resource
+   
    def index
       @posts = Post.all
       
