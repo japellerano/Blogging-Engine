9506aed3623f8dbd740fbf2b58309450b4091fe7
James Pellerano <james@japellerano.com>
2011-12-25 09:39:06 -0500

    added comments controller and model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
    validates :name, :presence => true
    validates :title, :presence => true, :length => {:minimum => 5}
    validates :excerpt, :presence => false, :length => {:maximum => 600}
+   
+   has_many :comments, :dependent => :destroy
 end
